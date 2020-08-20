1455318aded69400ab4d5a07e554669718babb59
LIU_YIFENG <liuyifeng.smc@gmail.com>
2020-08-20 13:51:31 +0800

    Duke_Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,23 @@
+import javax.script.ScriptContext;
+import java.util.Scanner;
+
 public class Duke {
-
-    public static void Greet(){
-
-        String greeting ="Hello! I'm Duke\n"
-                + "What can I do for you?\n"
-                + "-------------------------------------------\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "-------------------------------------------\n";
+    public static void printLine(){
         System.out.println("-------------------------------------------");
+    }
+    public static void printBye(){
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+    public static void printGreeting(){
+        String greeting ="Hello! I'm Duke\n"
+                + "What can I do for you?\n";
         System.out.print(greeting);
+        printLine();
+        //printBye();
+        //printLine();
+    }
+    public static void printEcho(String input){
+
     }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -17,7 +26,18 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
-        Greet();
+        printGreeting();
+        System.out.println("Please enter your command:");
+        String input;
+        Scanner in = new Scanner(System.in);
+        input = in.nextLine();
+        while(true){
+            if(input.equals("bye")){
+                break;
+            }
+            System.out.println(input + "~~");
+            input = in.nextLine();
+        }
+        printBye();
     }
 }
