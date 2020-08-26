becf70cc403aa8ebd21f92e2cd5dfe6f22560eef
LIU_YIFENG <liuyifeng.smc@gmail.com>
2020-08-26 10:03:44 +0800

    updated Class Task for Level-3

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Task {
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description){ //setting up the constructor
+        this.description = description;
+        this.isDone = false;
+    }
+    public String getStatusIcon(){
+
+        return (isDone ? "\u2713" : "\u2718"); //return tick or cross symbols
+    }
+    public boolean makeAsDone(){
+
+        return isDone = true;
+    }
+
 }
