4a881af71526725cdcd08778eb6ed86ceca796eb
LIU_YIFENG <liuyifeng.smc@gmail.com>
2020-09-03 14:03:19 +0800

    update after PR review

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,3 @@
-import javax.script.ScriptContext;
-import javax.sound.midi.SysexMessage;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -38,56 +36,33 @@ public class Duke {
         Task tasks[] = new Task[MAX_CAPACITY];
         Scanner in = new Scanner(System.in);
         input = in.nextLine();
-        while(!input.equals("bye")){
-            if(input.equals("list")){
-                if(list.size()==0 ){
-                    System.out.println("The list is empty");
-                }else {
-                    printList(list, tasks);
-                    printLine();
-                }
-            }else if(input.contains("done")){
-                if(list.size()==0) {
-                    System.out.println("The list is empty");
-                } else {
-                    String inputNumber;
-                    int taskNumber;
-                    inputNumber = input.replaceAll("[^0-9]", "");//replace all non-number with space
-                    taskNumber = Integer.parseInt(inputNumber);
-                    if(taskNumber > list.size()){
-                        System.out.println("The task number is out of bound! Please type \"list\"");
-                        printLine();
-                    }else if(tasks[taskNumber-1].isDone==true){
-                        System.out.println("Chill man, this task is completed!");
-                        printLine();
-                    }else {
-                        tasks[taskNumber - 1].makeAsDone();
-                        printDoneMessage(tasks[taskNumber - 1]);
-                        printLine();
-                    }
-                }
-            } else if(input.contains("todo")){
+        while (!input.equals("bye")) {
+            if (input.equals("list")) {
+                printList(list, tasks);
+            } else if (input.startsWith ("done")) {
+                setDone(input, list, tasks);
+            } else if (input.startsWith ("todo")) {
                 input = input.replace("todo ", "");
                 list.add(input);
                 tasks[count] = new ToDo(input);
-                count = printTaskAdded(count, list, tasks);
-            }else if(input.contains("event")){
+                count = printAddTaskMessage (count, list, tasks);
+            } else if (input.startsWith ("event")) {
                 input = input.replace("event ", "");
                 int positionIndex = input.indexOf("/");
                 String date = input.substring(positionIndex+1, input.length());
                 input = input.substring(0, positionIndex);
                 tasks[count] = new Event(input, date);
                 list.add(tasks[count].getDescription());
-                count = printTaskAdded(count, list, tasks);
-            }else if(input.contains("deadline")) {
+                count = printAddTaskMessage (count, list, tasks);
+            } else if (input.startsWith ("deadline")) {
                 input = input.replace("deadline ", "");
                 int positionIndex = input.indexOf("/");
                 String date = input.substring(positionIndex + 1, input.length());
                 input = input.substring(0, positionIndex);
                 tasks[count] = new Deadline(input, date);
                 list.add(tasks[count].getDescription());
-                count = printTaskAdded(count, list, tasks);
-            } else{
+                count = printAddTaskMessage (count, list, tasks);
+            } else {
                 System.out.println("Please input valid task description......");
                 printLine();
             }
@@ -96,7 +71,47 @@ public class Duke {
         printBye();
     }
 
+    private static int printAddTaskMessage (int count, ArrayList<String> list, Task[] tasks) {
+        System.out.println("Got it. I've added this task:" );
+        System.out.println("  ["
+                + tasks[count].getType()
+                + "]"
+                + "[" + tasks[count].getStatusIcon()
+                + "] "
+                + list.get(count));
+        System.out.println("Now you have " + list.size() +" tasks in the list.");
+        printLine();
+        count++;
+        return count;
+    }
+
+    private static void setDone(String input, ArrayList<String> list, Task[] tasks) {
+        if (list.size()==0) {
+            System.out.println("The list is empty");
+            printLine();
+        } else {
+            String inputNumber;
+            int taskNumber;
+            inputNumber = input.replaceAll("[^0-9]", "");//replace all non-number with space
+            taskNumber = Integer.parseInt(inputNumber);
+            if (taskNumber > list.size()) {
+                System.out.println("The task number is out of bound! Please type \"list\"");
+                printLine();
+            } else if (tasks[taskNumber-1].isDone==true){
+                System.out.println("Chill man, this task is completed!");
+                printLine();
+            } else {
+                tasks[taskNumber - 1].makeAsDone();
+                printDoneMessage(tasks[taskNumber - 1]);
+                printLine();
+            }
+        }
+    }
+
     private static void printList(ArrayList<String> list, Task[] tasks) {
+        if (list.size()==0) {
+            System.out.println("The list is empty");
+        }
         for (int i = 0; i < list.size(); i++) {
             System.out.println(i+1
                     + ".["
@@ -105,6 +120,7 @@ public class Duke {
                     +"[" + tasks[i].getStatusIcon() + "] "
                     + list.get(i));
         }
+        printLine();
     }
 
     private static void printDoneMessage(Task task) {
@@ -138,17 +154,4 @@ public class Duke {
         printLine();
     }
 
-    private static int printTaskAdded(int count, ArrayList<String> list, Task[] tasks) {
-        System.out.println("Got it. I've added this task:" );
-        System.out.println("  ["
-                + tasks[count].getType()
-                + "]"
-                + "[" + tasks[count].getStatusIcon()
-                + "] "
-                + list.get(count));
-        System.out.println("Now you have " + list.size() +" tasks in the list.");
-        printLine();
-        count++;
-        return count;
-    }
 }
