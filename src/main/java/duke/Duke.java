552127cb63e213bb086887665f63736001d66353
LIU_YIFENG <liuyifeng.smc@gmail.com>
2020-09-14 11:58:11 +0800

    branch-Level-7

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -5,26 +5,34 @@ import duke.tasks.Event;
 import duke.tasks.Task;
 import duke.tasks.ToDo;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
 
     public static final int MAX_CAPACITY = 100;
+    public static final int INDEX_PAST_RECORD_STATUS = 4;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         printWelcomeMessage();
         decodingUserCommands();
     }
 
-    public static void printLine(){
+    private static void printLine() {
         System.out.println("----------------------------------"
                 +"-------------------------------------------");
     }
-    public static void printBye(){
+
+    private static void printBye() {
         System.out.println("Bye. Hope to see you again soon!");
     }
-    public static void printGreeting(){
+
+    private static void printGreeting() {
         String greeting ="Hello! I'm Duke\n"
                 + "What can I do for you?\n";
         System.out.print(greeting);
@@ -36,11 +44,28 @@ public class Duke {
      * Based on the input, new task of different types (e.g todo, event, deadline)
      * will be created accordingly
      */
-    private static void decodingUserCommands() {
+    private static void decodingUserCommands() throws IOException {
         String input;
-        int count = 0;
+        int listSize = 0;
         ArrayList<String> list = new ArrayList<String>();
         Task tasks[] = new Task[MAX_CAPACITY];
+
+        //importing pastRecord before reading user commands
+        printImportingMessage ();
+        String pastRecordPath = "D:\\NUS\\Year 2 Sem 1\\CS2113T\\iP\\iPoutput.txt";
+        File pastRecord = new File (pastRecordPath);
+        try {
+            Scanner scanner = new Scanner (pastRecord);
+            while (scanner.hasNextLine ()) {
+                listSize = importPastRecord (listSize, list, tasks, scanner);
+            }
+            printDoneLoadingMessage ();
+        } catch (FileNotFoundException e) {
+                System.out.println("There is no past record! You are a 1st time user");
+                printLine ();
+        }
+
+        //Reading user commands after importing pastRecord
         Scanner in = new Scanner(System.in);
         input = in.nextLine();
         while (!input.equals("bye")) {
@@ -49,62 +74,148 @@ public class Duke {
             } else if (input.startsWith ("done")) {
                 setDone(input, list, tasks);
             } else if (input.startsWith ("todo")) {
-                if (input.equals ("todo")) {
-                    System.out.println("\u2639 " + "OOPS!!! The description of a todo cannot be empty.");
-                    printLine ();
-                } else {
-                    input = input.replace ("todo ", "");
-                    list.add (input);
-                    tasks[count] = new ToDo (input);
-                    count = printAddTaskMessage (count, list, tasks);
-                }
+                listSize = setTodo (input, listSize, list, tasks);
             } else if (input.startsWith ("event")) {
-                try {
-                    input = input.replace ("event ", "");
-                    int positionIndex = input.indexOf ("/");
-                    String date = input.substring (positionIndex + 1, input.length ());
-                    input = input.substring (0, positionIndex);
-                    tasks[count] = new Event (input, date);
-                    list.add (tasks[count].getDescription ());
-                    count = printAddTaskMessage (count, list, tasks);
-                } catch (StringIndexOutOfBoundsException s) {
-                    System.out.println ("\u2639 " + "OOPS!!! The description of an event cannot be empty.");
-                    printLine ();
-                }
+                listSize = setEvent (input, listSize, list, tasks);
             } else if (input.startsWith ("deadline")) {
-                try {
-                    input = input.replace ("deadline ", "");
-                    int positionIndex = input.indexOf ("/");
-                    String date = input.substring (positionIndex + 1, input.length ());
-                    input = input.substring (0, positionIndex);
-                    tasks[count] = new Deadline (input, date);
-                    list.add (tasks[count].getDescription ());
-                    count = printAddTaskMessage (count, list, tasks);
-                } catch (StringIndexOutOfBoundsException s) {
-                    System.out.println("\u2639 " + "OOPS!!! The description of a deadline cannot be empty.");
-                    printLine ();
-                }
+                listSize = setDeadline (input, listSize, list, tasks);
             } else {
                 System.out.println("\u2639 " + "OOPS!!! I'm sorry, but I don't know what that means :-(");
                 printLine();
             }
             input = in.nextLine();
         }
+        //when switching off the programme, export the tasks as output text and print bye message
+        exportTasksAsOutputText (list, tasks);
         printBye();
     }
 
-    private static int printAddTaskMessage (int count, ArrayList<String> list, Task[] tasks) {
+    private static int importPastRecord (int listSize, ArrayList<String> list, Task[] tasks, Scanner scanner) {
+        String content = scanner.nextLine ();
+        int pastTaskStatus = Integer.parseInt (content.substring (INDEX_PAST_RECORD_STATUS, INDEX_PAST_RECORD_STATUS+1));
+        if (content.startsWith ("T")) { //pastRecord has a todo task and add the todo to the task list
+            String todoDescription = "todo " + content.substring (INDEX_PAST_RECORD_STATUS + 4, content.length ());
+            listSize = setTodo (todoDescription, listSize, list, tasks);
+            if (pastTaskStatus == 1) { //if pastTaskStatus is 1, mark it done
+                tasks[listSize - 1].makeAsDone ();
+            }
+        } else if (content.startsWith ("E")) { //pastRecord has a event task and add the event to the task list
+            String eventDescription = "event " + content.substring (INDEX_PAST_RECORD_STATUS + 4, content.length () - 1);
+            eventDescription = eventDescription.replace ("(", "/");
+            listSize = setEvent (eventDescription, listSize, list, tasks);
+            if (pastTaskStatus == 1) { //if pastTaskStatus is 1, mark it done
+                tasks[listSize - 1].makeAsDone ();
+            }
+        } else if (content.startsWith ("D")){ //pastRecord has a deadline task and add the deadline to the task list
+            String deadlineDescription = "event " + content.substring (INDEX_PAST_RECORD_STATUS + 4, content.length () - 1);
+            deadlineDescription = deadlineDescription.replace ("(", "/");
+            listSize = setEvent (deadlineDescription, listSize, list, tasks);
+            if (pastTaskStatus == 1) { //if pastTaskStatus is 1, mark it done
+                tasks[listSize - 1].makeAsDone ();
+            }
+        }
+        content = "";
+        return listSize;
+    }
+
+    private static void printDoneLoadingMessage () {
+        System.out.println("Done loading past record onto your task list!\n"
+                +"Updating task status based on past record\n"
+                +"updating...\n"
+                +".\n"
+                +".\n"
+                +"Done updating! Please type \"List\" to check!");
+        printLine ();
+    }
+
+    private static void printImportingMessage () {
+        System.out.println("Importing data from previous record"
+                + System.lineSeparator()
+                +"." + System.lineSeparator()
+                +"." + System.lineSeparator()
+                +"." + System.lineSeparator()
+                +"." + System.lineSeparator()
+                +"please wait :)");
+        printLine ();
+    }
+
+    private static void exportTasksAsOutputText (ArrayList<String> list, Task[] tasks) throws IOException {
+        //need to change the filePath as the user's filePath
+        String filePath = "D:\\NUS\\Year 2 Sem 1\\CS2113T\\iP\\iPoutput.txt";
+        File file = new File(filePath);
+        FileWriter fileWriter = new FileWriter (file.getAbsoluteFile ());
+        BufferedWriter bufferedWriter = new BufferedWriter (fileWriter);
+        if (list.size () == 0) {
+            bufferedWriter.write ("");
+        } else {
+            for (int i = 0; i < list.size (); i++) {
+                if (tasks[i].getStatusIcon () == "\u2713") {
+                    bufferedWriter.write (tasks[i].getType () + " | " + "1" + " | " + list.get (i) + "\n");
+                } else {
+                    bufferedWriter.write (tasks[i].getType () + " | " + "0" + " | " + list.get (i) + "\n");
+                }
+            }
+        }
+        bufferedWriter.close ();
+    }
+
+    private static int setDeadline (String input, int listSize, ArrayList<String> list, Task[] tasks) {
+        try {
+            input = input.replace ("deadline ", "");
+            int positionIndex = input.indexOf ("/");
+            String date = input.substring (positionIndex + 1, input.length ());
+            input = input.substring (0, positionIndex);
+            tasks[listSize] = new Deadline (input, date);
+            list.add (tasks[listSize].getDescription ());
+            listSize = printAddTaskMessage (listSize, list, tasks);
+        } catch (StringIndexOutOfBoundsException s) {
+            System.out.println("\u2639 " + "OOPS!!! The description of a deadline cannot be empty.");
+            printLine ();
+        }
+        return listSize;
+    }
+
+    private static int setEvent (String input, int listSize, ArrayList<String> list, Task[] tasks) {
+        try {
+            input = input.replace ("event ", "");
+            int positionIndex = input.indexOf ("/");
+            String date = input.substring (positionIndex + 1, input.length ());
+            input = input.substring (0, positionIndex);
+            tasks[listSize] = new Event (input, date);
+            list.add (tasks[listSize].getDescription ());
+            listSize = printAddTaskMessage (listSize, list, tasks);
+        } catch (StringIndexOutOfBoundsException s) {
+            System.out.println ("\u2639 " + "OOPS!!! The description of an event cannot be empty.");
+            printLine ();
+        }
+        return listSize;
+    }
+
+    private static int setTodo (String input, int listSize, ArrayList<String> list, Task[] tasks) {
+        if (input.equals ("todo")) {
+            System.out.println("\u2639 " + "OOPS!!! The description of a todo cannot be empty.");
+            printLine ();
+        } else {
+            input = input.replace ("todo ", "");
+            list.add (input);
+            tasks[listSize] = new ToDo (input);
+            listSize = printAddTaskMessage (listSize, list, tasks);
+        }
+        return listSize;
+    }
+
+    private static int printAddTaskMessage (int listSize, ArrayList<String> list, Task[] tasks) {
         System.out.println("Got it. I've added this task:" );
         System.out.println("  ["
-                + tasks[count].getType()
+                + tasks[listSize].getType()
                 + "]"
-                + "[" + tasks[count].getStatusIcon()
+                + "[" + tasks[listSize].getStatusIcon()
                 + "] "
-                + list.get(count));
+                + list.get(listSize));
         System.out.println("Now you have " + list.size() +" tasks in the list.");
         printLine();
-        count++;
-        return count;
+        listSize++;
+        return listSize;
     }
 
     private static void setDone(String input, ArrayList<String> list, Task[] tasks) {
