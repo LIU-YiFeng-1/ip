e18534f95f3339c805bde3c8968a2a2cfb777bc6
LIU_YIFENG <liuyifeng.smc@gmail.com>
2020-09-25 22:05:20 +0800

    Add Java Doc

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -3,15 +3,12 @@ package duke;
 import duke.messages.Messages;
 import duke.tasks.Parser;
 import duke.tasks.TaskList;
-
-
-
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Duke {
 
-    public static final String RECORD_PATH = "Duke_output.txt";//file path from root file
+    public static final String RECORD_PATH = "Duke_output.txt";
 
     public static void main(String[] args) throws IOException {
         TaskList taskList = new TaskList();
@@ -22,13 +19,24 @@ public class Duke {
     }
 
     /**
-     * Reads user input and adds it to the task list accordingly
-     * Based on the input, new task of different types (e.g todo, event, deadline)
-     * will be created accordingly
+     * Decodes user input and execute the program accordingly.
+     * 1. list - prints all tasks in the task list.
+     * 2. done - sets the status of the desired task as done.
+     * 3. delete - deletes a desired task based on task index.
+     * 4. todo - adds a new ToDo task.
+     * 5. event - adds a new Event task.
+     * 6. deadline - adds a new Deadline task.
+     * 7. find - prints all tasks containing the given find key word.
+     * 8. save - saves all tasks available to Duke_output.txt.
+     * 9. bye - exits the program and saves all tasks available to Duke_output.txt.
+     * Program is always running until user inputs "bye".
+     * p.s all other inputs will be ignored!
+     *
+     * @param taskList Task list which stores different types of tasks.
+     * @throws IOException If the Storage class fails to read / write data.
      */
     private static void decodingUserCommands(TaskList taskList) throws IOException {
         String userInput;
-    //using Parser.parser to make sense of user input and perform the task accordingly
         Scanner in = new Scanner(System.in);
 
         do {
