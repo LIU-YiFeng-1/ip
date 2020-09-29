9beb37610341e8531757968cef44059631c6cbdc
LIU_YIFENG <liuyifeng.smc@gmail.com>
2020-09-29 11:23:59 +0800

    Delete Duke_exception package

--- src/main/java/duke/exceptions/DukeExceptions.java
+++
@@ -1,9 +0,0 @@
-package duke.exceptions;
-
-public abstract class DukeExceptions extends Exception {
-    public DukeExceptions() {
-        super();
-    }
-
-    public abstract void printErrorMessage(String command);
-}
